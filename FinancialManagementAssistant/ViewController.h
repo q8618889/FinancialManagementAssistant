bc1c6d5c5118104b57a33f2ce3f933a5988dbb15
Boxer <boxer@BoxerdeiMac.lan>
2015-02-03 09:40:21 +0800

    Initial Commit

---
+++ FinancialManagementAssistant/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FinancialManagementAssistant
+//
+//  Created by Boxer on 15/2/3.
+//  Copyright (c) 2015年 Boxer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FinancialManagementAssistant/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FinancialManagementAssistant
+//
+//  Created by Boxer on 15/2/3.
+//  Copyright (c) 2015年 Boxer. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
